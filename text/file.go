04dd57892c464aa8014784d170c5089016ab503b
aa <aaa@qq.com>
2018-07-04 09:54:21 +0800

    添加创建目录方法

--- text/file.go
+++ text/file.go
@@ -8,6 +8,8 @@ package Text
 
 import (
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 //判断当前路径的文件是否存在
@@ -18,4 +20,30 @@ func IsExists(path string) bool {
 	} else {
 		return true
 	}
+}
+
+//创建目录
+func CreateDir(path string, filemode ...os.FileMode) bool {
+	var fmode os.FileMode = 0777
+	if len(filemode) > 0 {
+		fmode = filemode[0]
+	}
+	err := os.MkdirAll(path, fmode)
+	if err == nil {
+		return true
+	} else {
+		return false
+	}
+}
+
+//获取程序运行的路径
+func GetAppDir() (appDir string) {
+	var err error
+	appDir, err = filepath.Abs(filepath.Dir(os.Args[0]))
+	if err == nil {
+		appDir = strings.Replace(appDir, "\\", "/", -1) //将\替换成/
+	} else {
+		appDir = ""
+	}
+	return appDir
 }
